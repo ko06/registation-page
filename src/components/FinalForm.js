b31b5be7817e277da1ba03eca1541a7750833882
Selvaganapathi Kannan <selvaganapathi@mallow-tech.com>
2020-11-21 16:00:51 +0530

    text changes added

--- src/components/FinalForm.js
+++ src/components/FinalForm.js
@@ -12,12 +12,15 @@ class FinalForm extends Component {
         step: 1,
         step_one_fields: {
             full_name: '',
+            validate: false,
         },
         step_two_fields: {
             comapany_name: '',
+            validate: false,
         },
         step_final_fields: {
             otp: '',
+            validate: false,
         },
     }
 
@@ -36,9 +39,10 @@ class FinalForm extends Component {
         this.setState({
             step_final_fields: {
                 ...step_final_fields,
-                ...values
+                ...values,
+                validate: true,
             }
-        }, () => console.log(this.state));
+        })
     }
 
     getFinalStepValue = (values) => {
@@ -56,9 +60,11 @@ class FinalForm extends Component {
         this.setState({
             step_one_fields: {
                 ...step_one_fields,
-                ...values
+                ...values,
+                validate: true
             }
-        })
+        }, () => console.log(this.state))
+
     }
 
     getStepTwoValue = (values) => {
@@ -66,38 +72,56 @@ class FinalForm extends Component {
         this.setState({
             step_two_fields: {
                 ...step_two_fields,
-                ...values
+                ...values,
+                validate: true,
             }
         })
     }
 
     tabSwitch = (key) => {
-        this.setState({ step: +key });
+        // let { step } = this.state;
+        // if(step > +key){
+        //   this.setState({ step: +key });
+        // }   
         //console.log(key);
+    }
+
+    getTopTabIcon = (form, info, currentStep) => {
+        let { step } = this.state;
+        return(<div>
+            <span className={step === currentStep ? 'number active' : form.validate
+             ? 'number is-okay' : 'number'}>
+             { (form.validate && step !== currentStep) ? 'L' : currentStep}</span>
+             <span>{info}</span>
+        </div>)
     }
 
     render() {
         const { step, step_one_fields, step_two_fields, step_final_fields } = this.state;
         return (
             <Tabs defaultActiveKey="1" animated={false} activeKey={step.toString()} onChange={this.tabSwitch}>
-                <TabPane tab="Personal Details" key="1">
+                <TabPane tab={this.getTopTabIcon(step_one_fields, 'Personal details', 1)} key="1">
                     <div className={'form-area'}>
-                        {<><h1>  Add your personal details</h1>
-                            <h3> Lorem Ipsum is simply dummy text of the printing and typesetting industry</h3></>}
+                        {<div className={'form-header'}>
+                            <h1>Add your personal details</h1>
+                            <h4> Lorem Ipsum is simply dummy text of the printing and typesetting industry</h4></div>}
                         <StepOne {...step_one_fields} handleNextButton={this.handleNextButton} submittedValues={this.getStepOneValue} />
                     </div>
                 </TabPane>
-                <TabPane tab="Company Details" key="2">
-                    <div  className={'form-area'}>
-                        {<><h1>  Add your company details</h1>
-                            <h3> Lorem Ipsum is simply dummy text of the printing and typesetting industry</h3></>}
+                <TabPane tab={this.getTopTabIcon(step_two_fields, 'Company details', 2)} key="2">
+                    <div className={'form-area'}>
+                        {<div className={'form-header'}>
+                            <h1>  Add your company details</h1>
+                            <h4> Lorem Ipsum is simply dummy text of the printing and typesetting industry</h4></div>}
                         <StepTwo {...step_two_fields} handleNextButton={this.handleNextButton} handleBackButton={this.handleBackButton} submittedValues={this.getStepTwoValue} />
                     </div>
                 </TabPane>
-                <TabPane tab="Email Verification" key="3">
-                    <div  className={'form-area'}>
-                    {<><h1>Enter your OTP</h1>
-                        <h3> Lorem Ipsum is simply dummy text of the printing and typesetting industry</h3></>}
+                <TabPane tab={this.getTopTabIcon(step_final_fields, 'Email Verification', 3)}
+                    key="3">
+                    <div className={'form-area'}>
+                        {<div className={'form-header'}>
+                            <h1>Enter your OTP</h1>
+                            <h4> For your security, we need ti verify.We sent a 5-digit code to name@domain.com. Please enter it below</h4></div>}
                         <StepFinal {...step_final_fields} handleConfirmButton={this.handleConfirmButton} handleBackButton={this.handleBackButton} submittedValues={this.getFinalStepValue} />
 
                     </div>
--- src/components/FormStepOne.js
+++ src/components/FormStepOne.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Select, Radio } from 'antd';
+
+const { Option } = Select;
 
 const StepOne = Form.create({
-    name:'step_one'})( props => {
+    name: 'step_one'
+})(props => {
     const { getFieldDecorator, validateFields } = props.form;
     const validateInput = (e) => {
         e.preventDefault();
         validateFields((err, values) => {
-            if(!err) {
+            if (!err) {
                 props.submittedValues(values);
                 props.handleNextButton();
             }
@@ -15,12 +18,44 @@ const StepOne = Form.create({
     }
     return (
         <Form onSubmit={validateInput}>
-            <Form.Item label="Full Name">        
+            <Form.Item label="Full Name">
                 {getFieldDecorator('full_name', {
-                    rules: [{ required: true, message: 'Cannot be empty!'}],
+                    rules: [{ required: true, message: 'Cannot be empty!' }],
                     initialValue: props.full_name
                 })(<Input placeholder="Input your value here" />)}
             </Form.Item>
+            <Form.Item label={'Gender'}>
+                {getFieldDecorator('gender', {
+                    rules: [
+                        { required: true, message: 'Please select a property type' },
+                    ],
+                })(
+                    <Radio.Group>
+                        <Radio.Button value="male">Male</Radio.Button>
+                        <Radio.Button value="female">Female</Radio.Button>
+                        <Radio.Button value="other">Other</Radio.Button>
+                    </Radio.Group>,
+                )}
+            </Form.Item>
+            <Form.Item label={'State'}>
+                {getFieldDecorator('state', {
+                    rules: [
+                        { required: true, message: 'Please select a property type' },
+                    ],
+                })(
+                    <Select
+                        placeholder="Select property type"
+                        className={'form-item select-md'}
+                        suffixIcon={<i className={'flaticon-down-arrow'} />}
+                    >
+                        <Option value="tamilnadu">TamilNadu</Option>
+                        <Option value="Mumbai">Mumbai</Option>
+                        <Option value="Delhi">Delhi</Option>
+
+                    </Select>,
+                )}
+            </Form.Item>
+
             <Form.Item>
                 <Button type="primary" htmlType="submit">
                     Next
